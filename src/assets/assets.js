c48ce2418288c19e0f7182eef55e18dad870b674
Mikwuegbu <mstephen130@yahoo.com>
2024-06-03 13:14:59 +0100

    added assets

---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import iconclose from './shared/icon-close.svg';
+import iconhamburger from './shared/icon-hamburger.svg';
+import logo from './shared/logo.svg';
+import bghomeDesktop from './home/background-home-desktop.jpg';
+import bghomeTablet from './home/background-home-tablet.jpg';
+import bghomeMobile from './home/background-home-mobile.jpg';
+
+export {
+	iconclose,
+	iconhamburger,
+	logo,
+	bghomeDesktop,
+	bghomeTablet,
+	bghomeMobile,
+};
