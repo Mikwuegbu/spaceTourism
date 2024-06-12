36775c02d5e727b7746c33a9b676ddd7ef56aba6
Mikwuegbu <mstephen130@yahoo.com>
2024-06-12 15:08:58 +0100

    tablet destination

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -37,23 +37,23 @@ const Destination = () => {
 
 	return (
 		<div className="bg-bgMobileDestination bg-cover min-h-screen md:bg-bgTabletDestination lg:bg-bgDesktopDestination">
-			<div className="pt-36 px-4 text-white flex flex-col items-center">
-				<div className="font-balowCondensed text-base font-normal text-white">
+			<div className="pt-36 px-4 md:px-10 text-white flex flex-col items-center md:items-start">
+				<div className="font-balowCondensed text-base md:text-lg font-normal text-white">
 					<span className="font-bold opacity-25 pr-4">01</span>
 					PICK YOUR DESTINATION
 				</div>
 				<div className="py-6 flex flex-col items-center text-center">
 					<img
-						src={currentDestination.images.png}
+						src={currentDestination.images.webp}
 						alt={currentDestination.name}
-						className="h-36 w-36 my-6"
+						className="h-36 w-36 md:h-[300px] md:w-[300px] my-6"
 					/>
-					<div className="space-x-8 space-y-4">
+					<div className="space-x-8 space-y-4 md:space-y-8">
 						{state.destinations.map((destination, index) => (
 							<button
 								key={destination.name}
 								onClick={() => setDestination(index)}
-								className={`text-lightBlue font-balowCondensed font-normal py-2 ${
+								className={`text-lightBlue font-balowCondensed font-normal py-2 md:text-xl ${
 									destination.name == currentDestination.name
 										? 'text-white border-b-[3px]'
 										: ''
@@ -63,16 +63,16 @@ const Destination = () => {
 							</button>
 						))}
 					</div>
-					<div className="py-6 space-y-4">
-						<h1 className="font-Bellefair font-normal text-6xl">
+					<div className="py-6 md:py-8 space-y-4">
+						<h1 className="font-Bellefair font-normal text-6xl md:text-[80px]">
 							{currentDestination.name.toUpperCase()}
 						</h1>
-						<p className="font-balowCondensed text-lightBlue pb-2">
+						<p className="font-balowCondensed text-lightBlue pb-2 px-20">
 							{currentDestination.description}
 						</p>
-						<div className="border border-white opacity-25" />
+						<div className="border border-white opacity-25 md:mx-24" />
 					</div>
-					<div className="space-y-6">
+					<div className="space-y-6 md:space-y-0 md:flex space-x-64">
 						<div className="space-y-3">
 							<p className="font-balowCondensed font-normal text-lightBlue">
 								AVG. DISTANCE
