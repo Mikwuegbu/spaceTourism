2b4d4b4c7896f72f67cf6af43f88d081fb312196
Mikwuegbu <mstephen130@yahoo.com>
2024-06-13 16:02:55 +0100

    Crew Page Mobile

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -42,25 +42,41 @@ const Crew = () => {
 					<span className="font-bold opacity-25 px-4">02</span>
 					MEET YOUR CREW
 				</div>
-				<div>
-					<div className="flex flex-col items-center text-center">
-						<p>{currentCrew.role.toUpperCase()}</p>
-						<h2>{currentCrew.name.toUpperCase()}</h2>
-						<p>{currentCrew.bio}</p>
+				<div className="">
+					<div className="flex flex-col items-center text-center py-16">
+						<div className="space-y-1.5">
+							<p className="font-Bellefair font-normal text-lg opacity-50">
+								{currentCrew.role.toUpperCase()}
+							</p>
+							<h2 className="font-Bellefair font-normal text-2xl">
+								{currentCrew.name.toUpperCase()}
+							</h2>
+						</div>
+						<p className="font-balowRegular font-normal text-lightBlue py-6 mb-8">
+							{currentCrew.bio}
+						</p>
 						<div className="space-x-4">
 							{state.crew.map((crew, index) => {
 								return (
 									<button
 										key={crew.name}
 										onClick={() => setCrew(index)}
-										className="w-3 h-3 bg-white"
+										className={`w-2.5 h-2.5 rounded-full ${
+											state.currentIndex == index
+												? 'bg-white'
+												: 'bg-white opacity-20'
+										}`}
 									></button>
 								);
 							})}
 						</div>
 					</div>
-					<div>
-						<img src={currentCrew.images.webp} alt="" />
+					<div className="flex justify-center w-full py-6 px-6">
+						<img
+							src={currentCrew.images.webp}
+							alt=""
+							className="w-[271.24px] h-[340px]"
+						/>
 					</div>
 				</div>
 			</div>
