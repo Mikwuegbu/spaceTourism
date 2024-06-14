38a6affda3d895e1f7f1cc7e9710d3ed0bda8987
Mikwuegbu <mstephen130@yahoo.com>
2024-06-14 12:21:05 +0100

    crew Page Responsive

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -37,13 +37,13 @@ const Crew = () => {
 
 	return (
 		<div className="bg-bgMobileCrew md:bg-bgTabletCrew lg:bg-bgDesktopCrew min-h-screen bg-cover">
-			<div className="pt-36 px-10 text-white">
-				<div className="font-balowCondensed text-base md:text-xl font-normal text-white">
+			<div className="pt-36 lg:pt-44 px-10 lg:px-40 text-white">
+				<div className="font-balowCondensed text-base md:text-xl font-normal text-white md:text-justify text-center">
 					<span className="font-bold opacity-25 px-4">02</span>
 					MEET YOUR CREW
 				</div>
-				<div className="md:px-20">
-					<div className="flex flex-col items-center text-center py-16 ">
+				<div className="md:px-20 lg:px-0 lg:flex lg:items-center lg:space-x-16">
+					<div className="flex flex-col items-center lg:items-start text-center lg:text-justify py-16 lg:px-8">
 						<div className="space-y-1.5 md:space-y-4">
 							<p className="font-Bellefair font-normal text-lg md:text-2xl opacity-50">
 								{currentCrew.role.toUpperCase()}
@@ -55,13 +55,13 @@ const Crew = () => {
 						<p className="font-balowRegular font-normal text-lightBlue py-6 mb-8">
 							{currentCrew.bio}
 						</p>
-						<div className="space-x-4">
+						<div className="space-x-4 lg:space-x-8 lg:pt-40">
 							{state.crew.map((crew, index) => {
 								return (
 									<button
 										key={crew.name}
 										onClick={() => setCrew(index)}
-										className={`w-2.5 h-2.5 rounded-full ${
+										className={`w-2.5 h-2.5 lg:w-4 lg:h-4 rounded-full ${
 											state.currentIndex == index
 												? 'bg-white'
 												: 'bg-white opacity-20'
@@ -71,11 +71,11 @@ const Crew = () => {
 							})}
 						</div>
 					</div>
-					<div className="flex justify-center w-full py-6 md:py-0 px-6">
+					<div className="flex justify-center w-full py-6 md:py-0 px-6 lg:py-12 lg:mx-8">
 						<img
 							src={currentCrew.images.webp}
 							alt=""
-							className="w-[271.24px] h-[340px] md:w-[446.74px] md:h-[560px]"
+							className="w-[271.24px] h-[340px] md:w-[446.74px] md:h-[560px] lg:w-[539.28px] lg:h-[676px]"
 						/>
 					</div>
 				</div>
