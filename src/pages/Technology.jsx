6f17c7df938fcd7d6eb567ae2688cc24750f1a2a
Mikwuegbu <mstephen130@yahoo.com>
2024-06-17 13:20:31 +0100

    desktop version

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -38,23 +38,23 @@ const Technology = () => {
 	return (
 		<div className="bg-bgMobileTechnology md:bg-bgTabletTechnology lg:bg-bgDesktopTechnology min-h-screen bg bg-cover">
 			<div className="pt-36">
-				<div className="font-balowCondensed text-center md:text-justify text-base font-normal text-white md:pl-10 md:text-xl">
+				<div className="font-balowCondensed text-center md:text-justify text-base font-normal text-white md:pl-10 md:text-xl lg:ml-[115px]">
 					<span className="font-bold opacity-25 px-3">03</span>
 					SPACE LAUNCH 101
 				</div>
-				<div className="py-20">
-					<div className="md:w-full">
+				<div className="py-20 lg:grid lg:grid-flow-col lg:pl-40">
+					<div className="md:w-full lg:order-3">
 						<img
 							src={currentTechnology.images.landscape}
 							alt=""
-							className="w-[768px] h-[258px] md:w-full md:h-full"
+							className="w-[768px] h-[258px] md:w-full md:h-full "
 						/>
 					</div>
-					<div className="space-x-4 py-8 flex justify-center">
+					<div className="space-x-4 py-8 flex justify-center lg:space-x-0 lg:space-y-8 lg:order-1 lg:flex-col">
 						{state.technologies.map((technology, index) => (
 							<button
 								key={technology.name}
-								className={`w-10 h-10 md:w-14 md:h-14 rounded-full ${
+								className={`w-10 h-10 md:w-14 md:h-14 lg:w-20 lg:h-20 rounded-full ${
 									state.currentIndex == index
 										? 'bg-white'
 										: 'text-white border border-gray-500 hover:border-white'
@@ -65,19 +65,16 @@ const Technology = () => {
 							</button>
 						))}
 					</div>
-					<div
-						className="text-center py-4 md:px-24
-					"
-					>
+					<div className="text-center py-4 md:px-24 lg:px-16 lg:pl-24 lg:order-2 lg:text-justify lg:content-center">
 						<div className="space-y-4">
-							<p className="text-white font-normal font-Bellefair opacity-50 text-lg md:text-2xl">
+							<p className="text-white font-normal font-Bellefair opacity-50 text-lg md:text-2xl lg:text-3xl">
 								THE TERMINOLOGY...
 							</p>
 							<h2 className="text-white font-Bellefair font-normal text-2xl md:text-5xl">
 								{currentTechnology.name.toUpperCase()}
 							</h2>
 						</div>
-						<p className="text-lightBlue font-balowRegular font-normal px-4 py-4">
+						<p className="text-lightBlue font-balowRegular font-normal px-4 lg:px-0 py-4 lg:text-lg">
 							{currentTechnology.description}
 						</p>
 					</div>
